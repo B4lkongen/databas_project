4e165355e47f9e87a1ffd6f0c9c2a8dde6fbbf4f
B4lkongen <134702771+B4lkongen@users.noreply.github.com>
2024-04-26 08:25:13 +0200

    Delete sql/innit_tables/users_create.sql

--- sql/innit_tables/users_create.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users(
-	userId INT AUTO_INCREMENT,
-    userRole VARCHAR(50) NOT NULL DEFAULT 'student',
-	userFName VARCHAR(50) NOT NULL,
-    userName VARCHAR(50),
-	PRIMARY KEY(userId)
-    );
