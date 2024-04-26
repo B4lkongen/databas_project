23c897fbda26bd7af0dd32b479797f5fdcfb4e57
B4lkongen <134702771+B4lkongen@users.noreply.github.com>
2024-04-26 08:24:37 +0200

    Delete sql/proc/add_course.sql

--- sql/proc/add_course.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`findus`@`192.120.12.220` PROCEDURE `add_course`(IN tName VARCHAR(50),
-								IN cName VARCHAR(50),
-								IN cHp INT,
-                                OUT ret INT)
-BEGIN
-INSERT INTO courses (teacherId, courseName, hp)
-VALUES (tName, cName, cHp);
-END
