50df2623fdad46e4ff363924ab418bc683b44bdc
B4lkongen <134702771+B4lkongen@users.noreply.github.com>
2024-05-27 13:54:59 +0200

    Add files via upload

--- code/sql.py
+++ code/sql.py
@@ -1,145 +1,325 @@
+#Adrian Adborn, Viktor Listi
 import mysql.connector
+
+# Backend functions ------------------------------------------------------------------------------
+
+
 def connect_to_db():
 	cnx = mysql.connector.connect(user='balko', password='DHjsKl!!l34', host='192.120.12.220', database='main')
 	mycursor = cnx.cursor()
 	return cnx, mycursor
 
 
-def check_user (uName):
+def check_user (user_name):
 	cnx, mycursor = connect_to_db() 
 	y = 0
-	ret = mycursor.callproc('check_user', [uName, y])
+	try:
+		ret = mycursor.callproc('check_user', [user_name, y])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
 	cnx.close()
 	return ret[1]
+
+
+# Admin actions ------------------------------------------------------------------------------
+ 
+    #Functions
+        #show_users - shows all users and their role
+		#check_user - Shows information about a user
+		#add_student - adds a student user
+		#remove_student - removes a student user
+		#add_teacher - adds a teacher user
+		#remove_teacher - removes a teacher user
+
+def show_users ():
+	cnx, mycursor = connect_to_db()
+	print('Users:')
+
+	try:
+		mycursor.execute("SELECT * FROM users")
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	for x in mycursor:
+		print(x)
+	print('')
+	cnx.close()
+	return 0
 
 
 def add_student ():
 	name = input('Name: ')
 	cnx, mycursor = connect_to_db() 
-	y = 0
-	result = mycursor.callproc('add_student', [name])
+
+	try:
+		result = mycursor.callproc('add_student', [name])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
 	cnx.commit()
-	print(result)
 	cnx.close()
+	return 0
+
 
 def add_teacher ():
 	name = input('Name: ')
-	cnx, mycursor = connect_to_db() 
-	result = mycursor.callproc('add_teacher', [name])
+	cnx, mycursor = connect_to_db()
+	
+	try:
+		result = mycursor.callproc('add_teacher', [name])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
 	cnx.commit()
-	print(result)
 	cnx.close()
+	return 0
+
+	
+def remove_user():
+	cnx, mycursor = connect_to_db()
+	user_name = input('user name: ')
+
+	try:
+		mycursor.callproc('remove_user', [user_name])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	cnx.commit()
+	cnx.close()
+	return 0
 
 
-def add_course (uName):
-	courseName = input('CourseName: ')
+# Teacher actions ------------------------------------------------------------------------------
+ 
+    #Functions
+        #show_your_courses - 
+		#show_students - 
+		#add_course - 
+		#remove_course -
+		#add_assignment - 
+		#remove_assignment - 
+		#add_registration - 
+		#remove_registration -
+		#add_grade -
 
+
+def show_your_courses(u_name):
+	cnx, mycursor = connect_to_db()
+	try:
+		mycursor.callproc('courses_teacher', [u_name])
+		for result in mycursor.stored_results():
+			courses = result.fetchall()
+		print("Id"+ "| " + "Name" + "       " + "Hp")
+		for i in courses:
+			print(str(i[1]) + " | " + i[0] + "   | " + str(i[2]))
+			print("  └─ Assignments")
+			mycursor.callproc('assignments_course', [i[0]])
+			for result in mycursor.stored_results():
+				assignments = result.fetchall()
+			for a in assignments:
+				print("  "+str(a[1]) + " | " + a[0] + "   | " + str(a[2]))
+	except mysql.connector.Error as e:
+		print("Something went wrong... Try again later or contact the administrator: {}".format(e))
+		cnx.close()
+		return 1
+
+	cnx.close()
+	return 0
+
+
+def show_students():
+	cnx, mycursor = connect_to_db()
+	print('Users:')
+
+	try:
+		mycursor.callproc('show_students', [])
+	except mysql.connector.Error as e:
+		print("Something went wrong... Try again later or contact the administrator{}".format(e))
+		cnx.close()
+		return 1
+	
+	for result in mycursor.stored_results():
+		print(result.fetchall())
+	print('')
+	cnx.close()
+	return 0
+
+
+def add_course (user_name):
+	course_name = input('CourseName: ')
 	cnx, mycursor = connect_to_db()
 	y = 0
-	mycursor.callproc('add_course', [uName,courseName, y])
+
+	try:
+		mycursor.callproc('add_course', [user_name,course_name,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
 	cnx.commit()
 	for result in mycursor.stored_results():
 		print(result.fetchall())
 	cnx.close()
+	return 0
+
+
+def remove_course ():
+	cnx, mycursor = connect_to_db()
+
+	courseName = input('Course Name: ')
+
+	try:
+		mycursor.callproc('remove_course', [courseName,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	cnx.commit()
+	cnx.close()
+	return 0
+
+
+def add_assignment ():
+	cnx, mycursor = connect_to_db()
+
+	aName = input('Assignment Name: ')
+	courseId = input('Course Id: ')
+	hp = input("How many hp is the assignment? ")
+
+	try:
+		results = mycursor.callproc('add_assignment', [courseId, aName, hp,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	print('Added '+aName+' to course '+str(courseId) +' hp = '+ str(hp))
+	cnx.commit()
+	cnx.close()
+	return 0
+
+
+def remove_assignment ():
+	cnx, mycursor = connect_to_db()
+
+	assignment_name = input('Assignment Name: ')
+	course_name = input('Course Name: ')
+	
+	try:
+		mycursor.callproc('remove_assignment', [assignment_name,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	cnx.commit()
+	cnx.close()
+	return 0
 
 
 def add_registration ():
-	uName = input('UserName: ')
-	courseId = input('CourseId: ')
-
 	cnx, mycursor = connect_to_db()
-	y = 0
-	results = mycursor.callproc('add_registration', [uName,courseId, y])
-	print('Added '+uName+' to '+str(courseId) +' = '+ str(y))
-	cnx.commit()
 
-	print(results)
-	cnx.close()
-
-def add_assignment ():
-	aName = input('AssignmentName: ')
-	courseId = input('CourseId: ')
-
-	cnx, mycursor = connect_to_db()
-	y = 0
-	results = mycursor.callproc('add_registration', [courseId, aName, y])
-	print('Added '+aName+' to '+str(courseId) +' = '+ str(y))
-	cnx.commit()
-	cnx.close()
-
-
-def show_users ():
-	print('Users:')
-	cnx, mycursor = connect_to_db()
-	mycursor.execute("SELECT * FROM users")
-	for x in mycursor:
-		print(x)
-	print('')
-	cnx.close()
+	user_name = input('User Name: ')
+	courseId = input('Course Name: ')
 	
-def show_courses ():
-	print('Courses:')
-	cnx, mycursor = connect_to_db()
-	mycursor.execute("SELECT * FROM courses")
-	for x in mycursor:
-		print(x)
-	print('')
-	cnx.close()
-
-
-def show_your_courses (u_name):
-	cnx, mycursor = connect_to_db()
-	y = 0
-	mycursor.callproc('courses_teacher', [u_name])
+	try:
+		results = mycursor.callproc('add_registration', [user_name,courseId,0])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	print('Added '+user_name+' to course '+str(courseId))
 	cnx.commit()
-	for result in mycursor.stored_results():
-		print(result.fetchall())
 	cnx.close()
-	cnx.close()
+	return 0
 
 
-def show_assignments():
-	print('assignments:')
+def remove_registration ():
+	user_name = input('User Name: ')
+	courseId = input('Course name: ')
 	cnx, mycursor = connect_to_db()
-	mycursor.execute("SELECT * FROM assignments")
-	for x in mycursor:
-		print(x)
-	print('')
+
+	try:
+		results = mycursor.callproc('remove_registration', [courseId, user_name])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	print('Removed '+user_name+' from course '+str(courseId))
+	cnx.commit()
 	cnx.close()
+	return 0
 
 
-def show_registrations():
-	print('registrations:')
+def change_grade():
 	cnx, mycursor = connect_to_db()
-	mycursor.execute("SELECT * FROM registrations")
-	for x in mycursor:
-		print(x)
-	print('')
+
+	user_name = input('Students username: ')
+	assignment_id = input('AssignmentId: ')
+	grade = input("Grade(1 for U and 2 for G): ")
+	
+	try:
+		mycursor.callproc('change_grade', [user_name, assignment_id, grade,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
+	cnx.commit()
 	cnx.close()
+	return 0
+	
+
+#Student actions ------------------------------------------------------------------------------
+ 
+    #Functions
+        #show_all_student - 
+		#show_users - 
 
 
-def show_all_student(uName):
+def show_all_student(user_name):
 	print('Here are all youre courses and assignments: ')
 	cnx, mycursor = connect_to_db()
-	res = mycursor.callproc('student_get_data', [uName,])
-	cnx.close()
+
+	try:
+		mycursor.callproc('student_get_data', [user_name,])
+	except:
+		print("Something went wrong... Try again later or contact the administrator")
+		cnx.close()
+		return 1
+	
 	for result in mycursor.stored_results():
-		arr = result.fetchall()
+		arr = (result.fetchall())
 	parse_courses_assignments(arr)
 
+
 def parse_courses_assignments(arr):
-    prev_item = None
-    for item in arr:
-        if prev_item == None or prev_item[0] != item[0]:
-            prev_item = item
-            print(item[0]+' '+ str(item[1]))
-        print('└─'+item[2]+' '+ str(item[3])+' '+ str(item[4]))
-        prev_item = item
-			
-
-
-def show_all():
-	show_users()
-	show_courses()
-	show_registrations()
-	show_assignments()
+	prev_item = None
+	print("Name"+ "		| HP |	Grade(for assignments)")
+	for item in arr:
+		course = item[0]
+		course_hp = str(item[1])
+		assignment = item[2]
+		assignment_hp = str(item[3])
+		grade = "G" if item[4] == 2 else "U" if item[4] == 1 else "Not graded"
+		
+		if prev_item == None or prev_item[0] != item[0]:
+			prev_item = item
+			print(course+' | '+ course_hp)
+		print('└─'+assignment+' | '+ assignment_hp+' | '+ grade)
+		prev_item = item
--- code/testmain.py
+++ code/testmain.py
@@ -1,30 +1,31 @@
+#Adrian Adborn, Viktor Listi
 import sql
 import inquirer
 
 admin_command_help = [("Show all users", 1),
-                        ("Show all courses", 2),
-                        ("add teacher",3),
-                        ("add student",4),
+                        ("add a student",2),
+                        ("Add a teacher",3),
+                        ("remove a user",4),
                         ("Quit", 0)]
 
-teacher_command_help = [("Show all students", 1),
-                        ("Add course",3),
-                        ("Register student on course",4),
+teacher_command_help = [("Show your courses", 1),
+                        ("Show users",2),
+                        ("add a course",3),
+                        ("remove a course",4),
                         ("Add assignment to course",5),
-                        ("show your courses",6),
+                        ("Remove assignment to course",6),
+                        ("Register student on course",7),
+                        ("Deregister student on course",8),
+                        ("Grade assignment",9),
                         ("Quit", 0)]
 
-student_command_help = [("Show courses", 1),
-                        ("Add student",2),
-                        ("Add course",3),
-                        ("Register student on course",4),
-                        ("Add assignment to course",5),
+student_command_help = [("Show Your courses and assignments", 1),
                         ("Quit", 0)]
 
 def auth(userName):
     global current_user
     global current_role
-    current_user = userName
+    current_user = userName      
     current_role = sql.check_user(userName)
 
 
@@ -37,6 +38,7 @@ def run():
     elif current_role == 'student':
         choose_action_student()
 
+
 def choose_action_admin():
     keep_alive = True
     while keep_alive:
@@ -48,51 +50,51 @@ def choose_action_admin():
         elif command == 1:
             sql.show_users()
         elif command == 2:
-            sql.show_courses()
+            sql.add_student()
         elif command == 3:
             sql.add_teacher()
         elif command == 4:
-            sql.add_student()
+            sql.remove_user()
+
 
 def choose_action_teacher():
     keep_alive = True
     while keep_alive:
-        q = [inquirer.List("command", message="Here are the avalible commands\n", choices=teacher_command_help)]
+        q = [inquirer.List("command", message="", choices=teacher_command_help)]
+        a = inquirer.prompt(q)
+        command = a["command"]
+        if command == 0:
+            keep_alive = False
+        elif command == 1:
+            sql.show_your_courses(current_user)
+        elif command == 2:
+            sql.show_students()
+        elif command == 3:
+            sql.add_course(current_user)
+        elif command == 4:
+            sql.remove_course(current_user)
+        elif command == 5:
+            sql.add_assignment()
+        elif command == 6:
+            sql.remove_assignment()
+        elif command == 7:
+            sql.add_registration()
+        elif command == 8:
+            sql.remove_registration()
+        elif command == 9:
+            sql.change_grade()
+        
+
+def choose_action_student():
+    keep_alive = True
+    while keep_alive:
+        q = [inquirer.List("command", message="", choices=student_command_help)]
         a = inquirer.prompt(q)
         command = a["command"]
         if command == 0:
             keep_alive = False
         elif command == 1:
             sql.show_all_student(current_user)
-        elif command == 2:
-            sql.add_user()
-        elif command == 3:
-            sql.add_course(current_user)
-        elif command == 4:
-            sql.add_registration()
-        elif command == 5:
-            sql.add_assignment()
-        elif command == 6:
-            sql.show_your_courses(current_user)
-
-def choose_action_student(command):
-    keep_alive = True
-    while keep_alive:
-        q = [inquirer.List("command", message="Here are the avalible commands\n", choices=teacher_command_help)]
-        a = inquirer.prompt(q)
-        command = a["command"]
-        if command == 0:
-            keep_alive = False
-        elif command == 1:
-            sql.show_assignments()
-        elif command == 2:
-            sql.add_user()
-        elif command == 3:
-            sql.add_course(current_user)
-        elif command == 4:
-            sql.add_registration()
-        elif command == 5:
-            sql.add_assignment()
 
 
 global current_user
